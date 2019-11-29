2550af9645027d86c38431ba366a0810a202240a
Cary <CaryZhengMy@gmail.com>
2019-11-29 17:18:24 +0800

    Init example project

---
+++ Example-Audio/Example-Audio/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Example-Audio
+//
+//  Created by Cary on 2019/11/29.
+//  Copyright © 2019 Cary. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
