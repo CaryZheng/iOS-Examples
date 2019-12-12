48672909f79f55ff086efd2a090f1b44e7e3011b
Cary <CaryZhengMy@gmail.com>
2019-12-12 19:01:23 +0800

    Add flutter example project

---
+++ Example-Flutter/Example-Flutter/Example-Flutter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Example-Flutter
+//
+//  Created by Cary on 2019/12/12.
+//  Copyright © 2019 Cary. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
