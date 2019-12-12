4818a71b9409de903e082946fe00370fb0608f8f
Cary <CaryZhengMy@gmail.com>
2019-12-12 20:14:01 +0800

    Add flutter embed code

--- Example-Flutter/Example-Flutter/Example-Flutter/AppDelegate.swift
+++ Example-Flutter/Example-Flutter/Example-Flutter/AppDelegate.swift
@@ -7,14 +7,18 @@
 //
 
 import UIKit
+import FlutterPluginRegistrant
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    lazy var flutterEngine = FlutterEngine(name: "my flutter engine")
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        flutterEngine.run()
+        GeneratedPluginRegistrant.register(with: self.flutterEngine);
+        
         return true
     }
 
