0eb55d738bff5afb840a7f5d14912874f3b0914f
Cary <CaryZhengMy@gmail.com>
2020-06-09 14:51:00 +0800

    Init ZFramework project

---
+++ Example-ZFramework/Example-ZFramework/Example-ZFramework/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Example-ZFramework
+//
+//  Created by Cary on 2020/6/9.
+//  Copyright © 2020 Cary. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Example-ZFramework/Example-ZFramework/Example-ZFrameworkTests/Example_ZFrameworkTests.swift
@@ -0,0 +1,34 @@
+//
+//  Example_ZFrameworkTests.swift
+//  Example-ZFrameworkTests
+//
+//  Created by Cary on 2020/6/9.
+//  Copyright © 2020 Cary. All rights reserved.
+//
+
+import XCTest
+@testable import Example_ZFramework
+
+class Example_ZFrameworkTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
